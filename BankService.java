da03553bc48a075a4f36e3765ae72efa49d83aed
bhavanav9105 <bhavanav9105@gmail.com>
2026-01-30 06:45:08 +0530

    Add files via upload

---
+++ BankService.java
@@ -0,0 +1,40 @@
+package com.bnmit;
+
+public class BankService {
+	
+	static class BankAccount {
+
+		double bal;
+
+		BankAccount (double b) {
+
+			bal = b;
+		}
+
+		void deposit (double a) {
+
+			bal += a;
+		
+		}
+
+		void withdraw (double a) {
+
+			bal -= a;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		BankAccount acc = new BankAccount(5500);
+
+		acc.deposit(600);
+		System.out.println("bal after deposit : "+ acc.bal);
+
+
+		acc.withdraw(300);
+		System.out.println("bal after withdraw : "+ acc.bal);
+
+		System.out.println("final bal : " + acc.bal);
+	}
+}
+		
